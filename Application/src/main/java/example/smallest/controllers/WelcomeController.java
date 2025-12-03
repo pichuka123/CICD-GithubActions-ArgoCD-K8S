578f5b83096bc726e345c6dad715fd712599fe13
pichuka123 <prince.charu7@gmail.com>
2025-12-03 16:33:06 +0530

    adding manifest files

--- Application/src/main/java/example/smallest/controllers/WelcomeController.java
+++ Application/src/main/java/example/smallest/controllers/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = {"text/html"})
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET, produces = {"text/html"})
 	public @ResponseBody String helloWorld() {
 
 		return "<p style=\"font-size:24px; font-weight:bold;\">🎉 Congratulations DevOps Engineers!!! AWS DevOps is a recession-proof IT Career 💼🚀. All the best for your Bright future 🌟🎯</p>";
